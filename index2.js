1886ff9479ffb986ab68fe8fe136be32f54ee311
Gunnar Mein <gunnar@together.science>
2023-07-26 20:20:52 -0700

    umd

---
+++ index2.js
@@ -0,0 +1,45 @@
+// game elements   
+const scoreSpan = document.querySelector("#score");
+const startButton = document.querySelector("#start");
+const playButton = document.querySelector("#play");
+const scoreBox = document.querySelector("#scorebox");
+const finishButton = document.querySelector("#finish");
+
+startButton.addEventListener("click", start);
+playButton.addEventListener("click", score);
+finishButton.addEventListener("click", finish);
+
+// init and first event
+const apiKey = "i2slulN)U%7xvMoVACLSEYogOekNQoWE";
+const appId = "37675ac8-c0c0-42e9-8291-0f9529df5d47";
+GEMS.init({apiKey, appId}).then(()=>{
+    GEMS.event("Demo-GamePage");
+    startButton.disabled = false;
+});
+
+function start() {
+    GEMS.event("Demo-GameStarted");
+    scoreSpan.innerText = "0";
+    playButton.disabled = false;
+    scoreBox.disabled = false;
+    startButton.disabled = true;
+}
+
+function score() {
+    let n = Number(scoreSpan.innerText);
+    let nNew = Number(scoreBox.value);
+    if (isNaN(nNew)){
+        nNew = 0;
+    }
+    n += nNew;
+    scoreSpan.innerText = String(n);
+    finishButton.disabled = false;
+}
+
+function finish() {
+    GEMS.event("Demo-GameFinished", {value:Number(scoreSpan.innerText)});
+    playButton.disabled = true;
+    scoreBox.disabled = true;
+    finishButton.disabled = true;
+    startButton.disabled = false;
+}
